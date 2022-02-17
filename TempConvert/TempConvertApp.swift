ae4dcc1e21bfc9251e36acb23730b8d64e7dba2e
Bern <bernnystrom@gmail.com>
2022-02-17 15:32:34 +0800

    Initial Commit

---
+++ TempConvert/TempConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConvertApp.swift
+//  TempConvert
+//
+//  Created by Bern N on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct TempConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
